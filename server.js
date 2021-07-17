97be076a688eb4a2fa09fd46b6aa0491b03abb97
slowed <rootslowed@gmail.com>
2021-07-17 15:38:54 -0300

    server done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -8,6 +9,24 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.static(`${process.cwd()}/src/public`));
+app.use(express.urlencoded({
+  limit: "10mb",
+  extended: false,
+}));
+app.use(express.json());
+
+try {
+  await mongoose.connect(process.env.MONGOURI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log("database connected");
+} catch (error) {
+  console.log(error);
+}
+
+import rootRouter from "./src/routes/rootRouter.js";
+app.use("/", rootRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(`${process.cwd()}/src/views/index.html`)
